d5409a08fd390fee14cb90612e652fa0c1c911e0
Anatoliy Korovin <antkorwin@gmail.com>
2019-01-05 13:36:45 +1000

    increase a coverage by testing AssertGraph's corner case

--- src/test/java/com/antkorwin/springtestmongo/internal/expect/graph/AssertGraphTest.java
+++ src/test/java/com/antkorwin/springtestmongo/internal/expect/graph/AssertGraphTest.java
@@ -1,15 +1,17 @@
 package com.antkorwin.springtestmongo.internal.expect.graph;
 
-import com.antkorwin.springtestmongo.internal.expect.graph.AssertGraph;
-import com.antkorwin.springtestmongo.internal.expect.graph.Printer;
-import com.antkorwin.springtestmongo.internal.expect.graph.TestGraph;
+import com.antkorwin.commonutils.exceptions.InternalException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created on 10.12.2018.
@@ -97,5 +99,44 @@ class AssertGraphTest {
             new AssertGraph(new TestGraph(() -> graph, data, patterns)).doAssert();
         });
         System.out.println(error);
+    }
+
+
+    @Nested
+    @DisplayName("Serialization problems for building error texts")
+    class TestCornerCaseWithAProblemOfMapping {
+
+        @Test
+        void testWithWrongData() {
+            // Arrange
+            boolean[][] graph = {
+                // @formatter:off
+                {true,  false},
+                {false, false},
+                // @formatter:on
+            };
+            List<Map<String, Object>> data = Arrays.asList(
+                    ImmutableMap.of("id", new ObjectWithPrivateFields()),
+                    ImmutableMap.of("id", new ObjectWithPrivateFields()));
+
+            List<Map<String, Object>> patterns = Arrays.asList(
+                    ImmutableMap.of("value", "BAR-1"),
+                    ImmutableMap.of("value", "BAR-2"));
+
+            InternalException error = Assertions.assertThrows(InternalException.class, () -> {
+                // Act
+                new AssertGraph(new TestGraph(() -> graph, data, patterns)).doAssert();
+            });
+            // Assert
+            assertThat(error.getCause().getMessage()).contains("No serializer found for class");
+        }
+
+        /**
+         * this class unable to serialize through Jackson (by default),
+         * because has private field.
+         */
+        private class ObjectWithPrivateFields {
+            private int a = 123;
+        }
     }
 }
